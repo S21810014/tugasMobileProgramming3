27d8e65de782dd0c2219b8b4df9a762c508aa4ff
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-04-23 11:34:50 +0800

    added SignUp page

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Header } from '../../components/molecules'
+
+const SignUp = () => {
+    return (
+        <View>
+            <Header canGoBack title="Sign Up"/>
+        </View>
+    )
+}
+
+export default SignUp
