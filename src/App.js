fc45d8b1dff1809110ccb46389821c833163ff96
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-04-23 08:30:10 +0800

    added signIn page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { SplashScreen } from './pages'
+import { SignIn, SplashScreen } from './pages'
 
 const App = () => {
     return (
         <View>
             <Text>Timothy Merfry Tiwow</Text>
             <SplashScreen/>
+            <SignIn/>
         </View>
     )
 }
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>Ini adalah signIn</Text>
+        </View>
+    )
+}
+
+export default SignIn
