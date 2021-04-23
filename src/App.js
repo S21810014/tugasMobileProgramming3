00f87549485672c5a17032b4968471e3577fd52e
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-04-23 08:04:53 +0800

    init setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>Timothy Merfry Tiwow</Text>
+        </View>
+    )
+}
+
+export default App
