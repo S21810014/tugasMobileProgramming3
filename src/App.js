c9e23d3a95883d7a1b1e1fc0414eba3f6b39afb0
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-04-23 08:12:22 +0800

    added more useless folders because its a requirement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { SplashScreen } from './pages'
 
 const App = () => {
     return (
         <View>
             <Text>Timothy Merfry Tiwow</Text>
+            <SplashScreen/>
         </View>
     )
 }
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>Ini adalah splashScreen</Text>
+        </View>
+    )
+}
+
+export default SplashScreen
