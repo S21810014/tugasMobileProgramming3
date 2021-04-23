b6754af2471c681a01eb08e6d69e2e376648c259
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-04-23 12:40:54 +0800

    added home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import { SignIn, SignUp, SplashScreen } from '../pages';
+import { Home, SignIn, SignUp, SplashScreen } from '../pages';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +21,11 @@ const Router = () => {
             <Stack.Screen 
                 name="SignUp" 
                 component={SignUp}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="Home"
+                component={Home}
                 options={{headerShown: false}}
             />
         </Stack.Navigator>
